1a1893d08a36b985f0c3907e5c319aec76180667
Alexandre <alexandre.dalabona@totvs.com.br>
2019-12-06 14:04:17 -0300

    Ajuste saveDatabase

--- utils.js
+++ utils.js
@@ -3,8 +3,7 @@ const path = require('path')
 
 module.exports = {
     saveDatabase(data, database) {
-        console.log((path.resolve(__dirname, 'data', `${database}.json`)));
         fs.writeFile(path.resolve(__dirname, 'data', `${database}.json`), 
                      JSON.stringify(data, null, 4), () => {});
     }
-}
+}   
